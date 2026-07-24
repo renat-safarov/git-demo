4e94bfd5e96e314fdd5e219c67d6992b1b50d96d
Renat Safarov <safarovrenat1995@gmail.com>
2026-07-24 20:54:33 +0300

    Hotfix

--- src/DemoApp.java
+++ src/DemoApp.java
@@ -1,5 +1,11 @@
 public class DemoApp {
     public static void main(String[] args) {
         System.out.println("Git demo");
+        System.out.println("Hello from dev");
+        Product product = new Product();
+        product.name = "Laptop";
+        product.price = 100.50;
+        product.quantity = 10;
+        System.out.println(product);
     }
 }
---
+++ src/Product.java
@@ -0,0 +1,14 @@
+public class Product {
+    public String name;
+    public double price;
+    public int quantity;
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "name='" + name + '\'' +
+                ", price=" + price +
+                ", quantity=" + quantity +
+                '}';
+    }
+}
