3f3c94f7c9a1728c6822bb4f08e240b75a56c860
Renat Safarov <safarovrenat1995@gmail.com>
2026-07-24 20:36:13 +0300

    Print product data in 'main'

--- src/DemoApp.java
+++ src/DemoApp.java
@@ -2,5 +2,10 @@ public class DemoApp {
     public static void main(String[] args) {
         System.out.println("Git demo");
         System.out.println("Hello from dev");
+        Product product = new Product();
+        product.name = "Laptop";
+        product.price = 100.50;
+        product.quantity = 10;
+        System.out.println(product);
     }
 }
