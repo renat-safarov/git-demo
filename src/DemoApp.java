edb8f70c4e9405a9496b1a3d8a86379ff0187c48
Renat Safarov <safarovrenat1995@gmail.com>
2026-07-24 20:05:19 +0300

    Add changes to cause merge conflict

--- src/DemoApp.java
+++ src/DemoApp.java
@@ -1,5 +1,7 @@
 public class DemoApp {
     public static void main(String[] args) {
         System.out.println("Git demo");
+        System.out.println("Change1");
+        System.out.println("Change2");
     }
 }
